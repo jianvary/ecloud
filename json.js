e24cd9fea772ad66235d4527a74bafe635eaf3a9
jianvary <754549372@qq.com>
2017-12-05 15:59:48 +0800

    add3

--- json.js
+++ json.js
@@ -1,107 +1,110 @@
 var data = {
-    '2g':{
-        'cpu':'1核',
-        'leichun': '2g',
-        'kbs': '2M',
+    'twog':{
+        'cpu':'1c',
+        'mem': '2g',
+        'bandwidth': '2M',
         'city': {
             'beijingJson':{
-                'ucloud': 185,
+                'ucloud': 126,
                 'aliyun': 96,
-                'tecentyun': 0,
-                'jinshan': 111
+                'tecentyun': 125,
+                'ksyun': 111
             },
-            'beijing': [185,96,0,111],
+            'beijing': [126,96,125,111],
+
             'shanghaiJson':{
-                'ucloud': 185,
+                'ucloud': 126,
                 'aliyun': 96,
                 'tecentyun': 0,
-                'jinshan': 111
+                'ksyun': 111
             },
             'shanghai': [220,270,240,200],
+
             'guangzhouJson':{
                 'ucloud': 185,
                 'aliyun': 96,
                 'tecentyun': 0,
-                'jinshan': 111
+                'ksyun': 111
             },
             'guangzhou': [220,270,240,200],
-            'chengdouJson':{
+
+            'chengduJson':{
                 'ucloud': 185,
                 'aliyun': 96,
                 'tecentyun': 0,
-                'jinshan': 111
+                'ksyun': 111
             },
-            'chengdou': [220,270,240,200],
+            'chengdu': [220,270,240,200],
         }
     },
-    '4g':{
-        'cpu':'4核',
-        'leichun': '4g',
-        'kbs': '5M',
+    'fourg':{
+        'cpu':'4c',
+        'mem': '4g',
+        'bandwidth': '5M',
         'city': {
             'beijingJson':{
                 'ucloud': 185,
                 'aliyun': 96,
                 'tecentyun': 0,
-                'jinshan': 111
+                'ksyun': 111
             },
             'beijing': [185,96,0,111],
             'shanghaiJson':{
                 'ucloud': 185,
                 'aliyun': 96,
                 'tecentyun': 0,
-                'jinshan': 111
+                'ksyun': 111
             },
             'shanghai': [220,270,240,200],
             'guangzhouJson':{
                 'ucloud': 185,
                 'aliyun': 96,
                 'tecentyun': 0,
-                'jinshan': 111
+                'ksyun': 111
             },
             'guangzhou': [220,270,240,200],
-            'chengdouJson':{
+            'chengduJson':{
                 'ucloud': 185,
                 'aliyun': 96,
                 'tecentyun': 0,
-                'jinshan': 111
+                'ksyun': 111
             },
-            'chengdou': [220,270,240,200],
+            'chengdu': [220,270,240,200],
         }
     },
-    '8g':{
-        'cpu':'4核',
-        'leichun': '8g',
-        'kbs': '10M',
+    'eightg':{
+        'cpu':'4c',
+        'mem': '8g',
+        'bandwidth': '10M',
         'city': {
             'beijingJson':{
                 'ucloud': 185,
                 'aliyun': 96,
                 'tecentyun': 0,
-                'jinshan': 111
+                'ksyun': 111
             },
             'beijing': [185,96,0,111],
             'shanghaiJson':{
                 'ucloud': 185,
                 'aliyun': 96,
                 'tecentyun': 0,
-                'jinshan': 111
+                'ksyun': 111
             },
             'shanghai': [220,270,240,200],
             'guangzhouJson':{
                 'ucloud': 185,
                 'aliyun': 96,
                 'tecentyun': 0,
-                'jinshan': 111
+                'ksyun': 111
             },
             'guangzhou': [220,270,240,200],
-            'chengdouJson':{
+            'chengduJson':{
                 'ucloud': 185,
                 'aliyun': 96,
                 'tecentyun': 0,
-                'jinshan': 111
+                'ksyun': 111
             },
-            'chengdou': [220,270,240,200],
+            'chengdu': [220,270,240,200],
         }
     },
 };
